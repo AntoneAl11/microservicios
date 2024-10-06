53dcec824544d220e2ac9eabbe99e1aca4fc46f3
Antonio Aleman <aleman.antonio.95@gmail.com>
2024-10-05 23:29:19 -0600

    Cargando base de datos en register.js y creando User.js

--- auth-microservices/register.js
+++ auth-microservices/register.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const connectDB = require('./db');
+const User = require('./User');
+
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.post('/register', (req, res) => {
+connectDB();
+
+app.post('/register', async (req, res) => {
   const { username, password } = req.body;
-  // Aquí iría la lógica de registro, como almacenar el usuario en una base de datos.
-  res.status(200).send({ message: 'Registration successful' });
+  try {
+    const newUser = new User({ username, password });
+    await newUser.save();
+    res.status(200).send({ message: 'Registration successful' });
+  } catch (error) {
+    res.status(400).send({ message: 'Registration failed', error });
+  }
 });
 
 const PORT = process.env.PORT || 4000;
